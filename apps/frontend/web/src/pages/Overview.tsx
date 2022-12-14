e8fbf2c21975937dd1a4207a40528637c57b49ce
codelande <corentin.delande.1@gmail.com>
2022-12-14 15:05:36 +0100

    m

--- apps/frontend/web/src/pages/Overview.tsx
+++ apps/frontend/web/src/pages/Overview.tsx
@@ -4,7 +4,7 @@ import { Box, Image, Text } from '@chakra-ui/react';
 const Overview = () => {
   return (
     <Box>
-        <Text fontSize="xl">Overviewzbrtrtb</Text>
+        <Text fontSize="xl">Overviewzbrtefeffefertb</Text>
         <Image src="/overview.jpg"/>
     </Box>
   )
